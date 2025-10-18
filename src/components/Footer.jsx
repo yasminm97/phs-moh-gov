14b186764d055ff355e032478bb619c431b8b9ce
Y A S M I N <136091028+yasminm97@users.noreply.github.com>
2025-10-18 20:31:26 +0300

    add some fixes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function footer() {
+  return (
+    <div className="mt-6 text-xs text-gray-400 text-center leading-relaxed">
+      &copy; وزارة الصحة - قسم القطاع الصحي الخاص
+      <br />
+      <a className="underline text-gray-300" href="https://firewall-iq.com/">
+        شركة الجدار الناري لخدمات الأمن السيبراني
+      </a>
+      <br />
+      للدعم والمساعدة: +9647838200050 / +9647713366777
+    </div>
+  );
+}
